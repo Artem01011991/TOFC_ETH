7b735c038cf490f4d7ca433fd99c4a928308a74c
artem <artem010191@mail.ru>
2018-04-22 19:22:27 +0300

    some changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import ETH_state
+import core_api
 from decouple import config
 
-obj = ETH_state.IndexInfo(config('USER_LOGIN'), config('USER_PASS'), config('USER_WMID'))
+obj = core_api.IndexInfo(config('USER_LOGIN'), config('USER_PASS'), config('USER_WMID'))
 print(obj.get_eth_status())
