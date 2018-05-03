e18d44501a70a5e4615b785e03d23df6f1f6a1fe
artem <artem010191@mail.ru>
2018-05-03 07:55:14 +0300

    some adds

--- core_binance_api.py
+++ core_binance_api.py
@@ -213,10 +213,66 @@ class BinanceCoreApi:
 
         return request('get', path.join(self.head, body), params=data).json()
 
+    def hr24_ticker_price_change_statistics(self):
+        """
+
+        :return: {
+                  "symbol": "BNBBTC",
+                  "priceChange": "-94.99999800",
+                  "priceChangePercent": "-95.960",
+                  "weightedAvgPrice": "0.29628482",
+                  "prevClosePrice": "0.10002000",
+                  "lastPrice": "4.00000200",
+                  "lastQty": "200.00000000",
+                  "bidPrice": "4.00000000",
+                  "askPrice": "4.00000200",
+                  "openPrice": "99.00000000",
+                  "highPrice": "100.00000000",
+                  "lowPrice": "0.10000000",
+                  "volume": "8913.30000000",
+                  "quoteVolume": "15.30000000",
+                  "openTime": 1499783499040,
+                  "closeTime": 1499869899040,
+                  "fristId": 28385,   // First tradeId
+                  "lastId": 28460,    // Last tradeId
+                  "count": 76         // Trade count
+                }
+        """
+        body = 'api/v1/ticker/24hr'
+
+        return request('get', path.join(self.head, body), params={'symbol': self.symbol}).json()
+
+    def symbol_price_ticker(self):
+        """
+
+        :return: {
+                  "symbol": "LTCBTC",
+                  "price": "4.00000200"
+                }
+        """
+        body = 'api/v3/ticker/price'
+
+        return request('get', path.join(self.head, body), params={'symbol': self.symbol}).json()
+
+    def symbol_order_book_ticker(self):
+        """
+
+        :return: {
+                  "symbol": "LTCBTC",
+                  "bidPrice": "4.00000000",
+                  "bidQty": "431.00000000",
+                  "askPrice": "4.00000200",
+                  "askQty": "9.00000000"
+                }
+        """
+        body = 'api/v3/ticker/bookTicker'
+
+        return request('get', path.join(self.head, body), params={'symbol': self.symbol}).json()
+
 if __name__=='__main__':
     o = BinanceCoreApi(
         config('BINANCE_APIKEY'),
         config('BINANCE_SECRETKEY'),
         'ETHBTC'
     )
-    print(o.kline_candlestick_data('1M'))
+    print(o.symbol_order_book_ticker())
