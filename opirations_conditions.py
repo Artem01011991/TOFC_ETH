205daefe13f79390d5938bf31e805e4222c64f4c
artem <artem010191@mail.ru>
2018-05-06 15:27:01 +0300

    s

--- opirations_conditions.py
+++ opirations_conditions.py
@@ -2,37 +2,6 @@ import settings
 import functools
 
 
-def sell(user_info, instrument_info):
-    user_notes = user_info['portfolio'][0]['notes']  # amount ETH notes
-    price = instrument_info['price'] + settings.PERCENT_SELL  # price for sale
-
-    if user_notes > settings.NOTES_AMOUNT:
-        return {
-            'count': settings.NOTES_AMOUNT,
-            'price': price,
-        }
-    elif user_notes:
-        return {
-            'count': user_notes,
-            'price': price,
-        }
-
-
-def buy(user_info, instrument_info):
-    user_wmz = user_info['balance']['wmz']  # amount WMZ
-    price = instrument_info['price'] - settings.PERCENT_BUY  # price for buy
-
-    if user_wmz >= settings.NOTES_AMOUNT * price:
-        return {
-            'count': settings.NOTES_AMOUNT,
-            'price': price,
-        }
-    elif user_wmz >= price:
-        return {
-            'count': int(user_wmz/price),
-            'price': price,
-        }
-
 class TradingOpirations:
 
     def __init__(self, instrument_info, user_info, my_offer_list):
