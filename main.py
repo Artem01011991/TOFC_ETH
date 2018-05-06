5aa731d0afe37203508a30b9da4e1f207b424ca1
artem <artem010191@mail.ru>
2018-05-06 15:17:36 +0300

    s

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import core_api
+import opirations_conditions
 import settings
 from decouple import config
-from opirations_conditions import sell, buy
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
@@ -11,13 +11,14 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def clock_sched():
     index_connection = core_api.IndexInfo(config('USER_LOGIN'), config('USER_PASS'), config('USER_WMID'))
-
     user_info = index_connection.get_balance()
     instrument_info = index_connection.get_eth_status()
     my_offer_list = index_connection.get_offer_my()
 
-    sell_condition = sell(user_info, instrument_info)
-    buy_condition = buy(user_info, instrument_info)
+    trading = opirations_conditions.TradingOpirations(instrument_info, user_info, my_offer_list)
+
+    sell_condition = trading.sell()
+    buy_condition = trading.buy()
 
     offer_for_delete = (
         i['offerid'] for i in my_offer_list
@@ -32,10 +33,10 @@ def clock_sched():
         for i in offer_for_delete:
             index_connection.delete_offer(i)
 
-    if not next((i for i in my_offer_list if i['price'] == sell_condition['price'] and i['kind'] == 0), None):  # sell operation
+    if sell_condition['count'] and not next((i for i in my_offer_list if i['price'] == sell_condition['price'] and i['kind'] == 0), None):  # sell operation
         index_connection.set_offer(sell_condition['count'], sell_condition['price'], is_bid=False)
 
-    if not next((i for i in my_offer_list if i['price'] == buy_condition['price'] and i['kind'] == 1), None):  # buy operation
+    if buy_condition['count'] and not next((i for i in my_offer_list if i['price'] == buy_condition['price'] and i['kind'] == 1), None):  # buy operation
         index_connection.set_offer(buy_condition['count'], buy_condition['price'])
 
 
--- opirations_conditions.py
+++ opirations_conditions.py
@@ -1,4 +1,5 @@
 import settings
+import functools
 
 
 def sell(user_info, instrument_info):
@@ -30,4 +31,38 @@ def buy(user_info, instrument_info):
         return {
             'count': int(user_wmz/price),
             'price': price,
+        }
+
+class TradingOpirations:
+
+    def __init__(self, instrument_info, user_info, my_offer_list):
+        self.buy_price = instrument_info['price'] - settings.PERCENT_BUY  # price for buy
+        self.sell_price = instrument_info['price'] + settings.PERCENT_SELL  # price for sale
+        self.user_wmz = user_info['balance']['wmz']  # amount WMZ
+        self.user_notes = user_info['portfolio'][0]['notes']  # amount ETH notes
+        self.selling_notes = functools.reduce(lambda x, y: x+y, (i['notes'] for i in my_offer_list if i['kind'] == 0))
+
+    def sell(self):
+        rest_notes = self.user_notes - self.selling_notes
+
+        if rest_notes > settings.NOTES_AMOUNT:
+            return {
+                'count': settings.NOTES_AMOUNT,
+                'price': self.sell_price,
+            }
+        return {
+            'count': rest_notes,
+            'price': self.sell_price,
+        }
+
+    def buy(self):
+
+        if self.user_wmz >= settings.NOTES_AMOUNT * self.buy_price:
+            return {
+                'count': settings.NOTES_AMOUNT,
+                'price': self.buy_price,
+            }
+        return {
+            'count': int(self.user_wmz / self.buy_price),
+            'price': self.buy_price,
         }
