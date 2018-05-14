27ebc5d47bb385c8d8661d7499cbf5029714b243
Артем <31074309+Artem01011991@users.noreply.github.com>
2018-05-14 12:45:15 +0300

    some adds

--- core_binance_api.py
+++ core_binance_api.py
@@ -463,10 +463,81 @@ class BinanceCoreApi:
 
         return request('get', path.join(self.head, body), params=data, headers=self.header).json()
 
+    def account_information(self):  # TODO check for work
+        """
+
+        :return:
+                {
+                  "makerCommission": 15,
+                  "takerCommission": 15,
+                  "buyerCommission": 0,
+                  "sellerCommission": 0,
+                  "canTrade": true,
+                  "canWithdraw": true,
+                  "canDeposit": true,
+                  "updateTime": 123456789,
+                  "balances": [
+                    {
+                      "asset": "BTC",
+                      "free": "4723846.89208129",
+                      "locked": "0.00000000"
+                    },
+                    {
+                      "asset": "LTC",
+                      "free": "4763368.68006011",
+                      "locked": "0.00000000"
+                    }
+                  ]
+                }
+        """
+        body = 'api/v3/account'
+        data = {'timestamp': self.timestamp}
+        signature = hmac.new(
+            config('BINANCE_SECRETKEY').encode(),
+            '&'.join(['{k}={v}'.format(k=k, v=v) for k, v in data.items()]).encode(),
+            digestmod=hashlib.sha256
+        ).hexdigest()
+
+        data.update({'signature': signature})
+
+        return request('get', path.join(self.head, body), params=data, headers=self.header).json()
+
+    def account_trade_list(self):  # TODO check for work
+        """
+
+        :return:
+                [
+                  {
+                    "id": 28457,
+                    "orderId": 100234,
+                    "price": "4.00000100",
+                    "qty": "12.00000000",
+                    "commission": "10.10000000",
+                    "commissionAsset": "BNB",
+                    "time": 1499865549590,
+                    "isBuyer": true,
+                    "isMaker": false,
+                    "isBestMatch": true
+                  }
+                ]
+        """
+        body = 'api/v3/myTrades'
+        data = {'symbol': self.symbol, 'timestamp': self.timestamp}
+        signature = hmac.new(
+            config('BINANCE_SECRETKEY').encode(),
+            '&'.join(['{k}={v}'.format(k=k, v=v) for k, v in data.items()]).encode(),
+            digestmod=hashlib.sha256
+        ).hexdigest()
+
+        data.update({'signature': signature})
+
+        return request('get', path.join(self.head, body), params=data, headers=self.header).json()
+
+
 if __name__=='__main__':
     o = BinanceCoreApi(
         config('BINANCE_APIKEY'),
         config('BINANCE_SECRETKEY'),
         'ETHBTC'
     )
-    print(o.current_open_orders())
+    print(o.account_information())
