6b6834a0a32a84247c6f48aa6dfd5b515c45dbb0
Artem <artem010191@mail.ru>
2019-12-28 20:08:27 +0300

    isort

--- tofc_eth/alembic/env.py
+++ tofc_eth/alembic/env.py
@@ -1,10 +1,9 @@
-from tofc_eth.conf import settings
-
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import create_engine
 
+from tofc_eth.conf import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
